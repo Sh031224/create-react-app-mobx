bfd926ebbeba565e17ac5026ed63db00c81c082a
차승호 <51149996+Sh031224@users.noreply.github.com>
2020-08-01 21:10:45 +0900

    Update MainPage.js

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,11 @@
 import React from "react";
-import ExampleTemplate from "components/common/template/ExampleTemplate";
 import ExampleContainer from "containers/Example/ExampleContainer";
 
 const ProfilePage = () => {
   return (
-    <ExampleTemplate>
+    <>
       <ExampleContainer />
-    </ExampleTemplate>
+    </>
   );
 };
 
